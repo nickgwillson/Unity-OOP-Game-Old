1a0f992d81214086eff721c5fd2fca49c72efd9c
Nicholas Willson <70346913+nickgwillson@users.noreply.github.com>
2021-11-17 18:46:30 -0700

    Add files via upload

---
+++ SideWalls.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWalls : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.name == "Ball")
+        {
+            string wallName = transform.name;
+            GameManager.Score(wallName);
+            hitInfo.gameObject.SendMessage("RestartGame", 1.0f, SendMessageOptions.RequireReceiver);
+        }
+    }
+}
+
